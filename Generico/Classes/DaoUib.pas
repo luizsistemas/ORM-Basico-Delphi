85d020c4d6b229c63cf35d3f2c8c90451f509a07
luizsistemas <contato@luizsistemas.com.br>
2014-10-08 10:04:28 -0300

    Update em DaoUib

--- Generico/Classes/DaoUib.pas
+++ Generico/Classes/DaoUib.pas
@@ -1,8 +1,44 @@
+{******************************************************************************}
+{ Projeto: ORM - Básico do Blog do Luiz                                        }
+{ Este projeto busca agilizar o processo de manipulação de dados (DAO/CRUD),   }
+{ ou seja,  gerar inserts, updates, deletes nas tabelas de forma automática,   }
+{ sem a necessidade de criarmos classes DAOs para cada tabela. Também visa     }
+{ facilitar a transição de uma suite de componentes de acesso a dados para     }
+{ outra.                                                                       }
+{                                                                              }
+{ Direitos Autorais Reservados (c) 2014 Luiz Carlos Alves                      }
+{                                                                              }
+{ Colaboradores nesse arquivo:                                                 }
+{    Luiz Carlos Alves - contato@luizsistemas.com.br                           }
+{                                                                              }
+{ Você pode obter a última versão desse arquivo no repositório                 }
+{ https://github.com/luizsistemas/ORM-Basico-Delphi                            }
+{                                                                              }
+{  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la }
+{ sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  }
+{ Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) }
+{ qualquer versão posterior.                                                   }
+{                                                                              }
+{ Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   }
+{ NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      }
+{ ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor}
+{ do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              }
+{                                                                              }
+{ Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto}
+{ com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  }
+{ no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          }
+{ Você também pode obter uma copia da licença em:                              }
+{ http://www.opensource.org/licenses/lgpl-license.php                          }
+{                                                                              }
+{ Luiz Carlos Alves - contato@luizsistemas.com.br -  www.luizsistemas.com.br   }
+{                                                                              }
+{******************************************************************************}
+
 unit DaoUib;
 
 interface
 
-uses Base, Rtti, Atributos, uib, system.SysUtils, System.Classes, uibdataset,
+uses prsBase, Rtti, PrsAtributos, uib, system.SysUtils, System.Classes, uibdataset,
   db;
 
 type
